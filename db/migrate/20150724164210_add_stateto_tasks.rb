70363f29bcdfb7b2fda74dce5e3ac4ff624c2c36
thelucasmoore <lucasedwardmoore@gmail.com>
2015-07-24 13:13:41 -0400

    Fixed States Controller

---
+++ db/migrate/20150724164210_add_stateto_tasks.rb
@@ -0,0 +1,5 @@
+class AddStatetoTasks < ActiveRecord::Migration
+  def change
+  	add_column :tasks, :state, :string, default: "to_do"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714211414) do
+ActiveRecord::Schema.define(version: 20150724164210) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "user_id"
     t.string   "integer"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
